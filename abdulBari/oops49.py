f7e791d4331f40c0462b022563d990dc23be3520
aksdhanju <137876753+aksdhanju@users.noreply.github.com>
2025-11-30 20:15:19 +0530

    basic tail command

--- abdulBari/oops49.py
+++ abdulBari/oops49.py
@@ -7,7 +7,8 @@ class Rectangle:
     def __init__(self, l, b):
         self.length = l
         self.breadth = b
-        Rectangle.count += 1
+        # Rectangle.count += 1
+        self.__class__.count += 1
     
     def area(self):
         return self.length * self.breadth
@@ -15,5 +16,37 @@ class Rectangle:
     def perimeter(self):
         return 2 * (self.length + self.breadth)
     
-r = Rectangle(10, 2)
-print(r.area())
+    # now we are writing a class method
+    # we want python to treat cls as class variable
+    # so write a decorator 
+    @classmethod
+    def get_count(cls):
+        return cls.count
+    
+r1 = Rectangle(10, 2)
+
+r2 = Rectangle(10, 2)
+print(r1.area())
+print(r1.get_count())
+print(r2.count) #count variable should be accessible without object also
+print(r1.count) #count variable should be accessible without object
+print(Rectangle.count)
+print(Rectangle.get_count())
+
+# if there was no annotation @classmethod, we can't call get_count method with
+# Rectangle.get_count()
+
+# count variable is common to both instances
+# instances can use class variable as a shared data 
+
+
+
+# 
+# Section 5: static methods
+# 
+
+
+
+# 
+# Section 6: property methods
+# 
---
+++ cli_apps/tail/tail_basic.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+A Python implementation of the tail command
+"""
+
+def tail_basic(filename, n=10):
+    """
+    Display the last n lines of a file
+    
+    Args:
+        filename: Path to the file
+        n: Number of lines to display (default: 10)
+    """
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            # Get last n lines
+            last_lines = lines[-n:]
+            
+            for line in last_lines:
+                print(line, end='')
+                
+    except FileNotFoundError:
+        print(f"tail: {filename}: No such file or directory")
+    except PermissionError:
+        print(f"tail: {filename}: Permission denied")
+
+# Test the function
+if __name__ == "__main__":
+    tail_basic("example.txt", 5)
